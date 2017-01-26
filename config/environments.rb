98987652bd36dd9312c7ef01b7d99e39c24c0ee6
Leland Krych <leland.krych@gmail.com>
2017-01-26 16:33:29 +0000

    change environment variable in config/environments

--- config/environments.rb
+++ config/environments.rb
@@ -4,7 +4,7 @@ end
  
 configure :production do
   # Database connection
-  db = URI.parse(ENV['postgres://rjhwwgczdbzcyb:4b59a0be0868b55bd46c617c01ee9d538b969d6ce9a0b0f69621ed2888fe51d7@ec2-23-21-111-81.compute-1.amazonaws.com:5432/d8jti2jer9ngoq'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
  
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
